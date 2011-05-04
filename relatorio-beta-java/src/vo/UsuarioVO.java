69521d9414c1580968d434029db4f2dfdee9acf9
Jeferson Santos <jeferfb@gmail.com>
2011-05-04 17:37:29 -0300

    change

---
+++ relatorio-beta-java/src/vo/UsuarioVO.java
@@ -0,0 +1,58 @@
+package vo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="usuarios")
+public class UsuarioVO {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String nome;
+	private String username;
+	private String password;
+	private String email;
+	private String role;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
